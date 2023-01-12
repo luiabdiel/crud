586a1af418df69273f62e57092a0346f38b1c217
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-01-12 15:19:17 -0300

    feat: Crud in blazor server

---
+++ crud/Data/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace crud.Data {
+    public class Employee {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ crud/Data/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿namespace crud.Data {
+    public interface IEmployeeService {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee(Employee employee);
+
+        void AddEmployee(Employee employee);
+
+        void DeleteEmployee(Guid id);
+    }
+}
